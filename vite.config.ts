17cd5a6cda06f602ae520ff9bb72b97141cf6370
Bram Verkuijlen <bram.verkuijlen.bv@gmail.com>
2024-10-01 15:15:30 +0200

    Vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {host: '127.0.0.1'},
+  test: {
+    environment: 'jsdom',
+    include: ['**/*.test.tsx'],
+  },
 })
